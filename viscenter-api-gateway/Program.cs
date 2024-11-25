8e122616c0f7362684bdec07e1c9251373ce68bd
luisarce02 <130387951+luisarce02@users.noreply.github.com>
2024-11-25 09:46:48 -0400

    feat: [apigateway] base integration of apigateway

---
+++ viscenter-api-gateway/Program.cs
@@ -0,0 +1,25 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
+var app = builder.Build();
+
+app.UseOcelot().Wait();
+
+app.Run();
